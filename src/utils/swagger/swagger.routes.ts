d49651f7e716ac434b9d04e24f3385344c3ac471
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-12-15 05:47:40 +0200

    APIDocu:refreshToken

--- src/utils/swagger/swagger.routes.ts
+++ src/utils/swagger/swagger.routes.ts
@@ -62,3 +62,29 @@
  *        '400':
  *              description: Bad request
  */
+
+
+/**
+ * @swagger
+ * /refresh-token:
+ *   post:
+ *     summary: Refresh Access Token
+ *     description: The refresh token is used to generate a new access token. Typically, if the access token has an expiration date, once it expires, the user would have to authenticate again to obtain an access token. It may also be necessary to generate a new access token when you want to access a resource that has not been accessed before.
+ *     tags:
+ *       - Authentication
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token containing the refresh token
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *        '200':
+ *              description: New access token generated successfully
+ *        '401':
+ *              description: Unauthorized - Invalid or expired refresh token
+ */
+
