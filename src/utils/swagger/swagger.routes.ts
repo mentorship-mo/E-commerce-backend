56ee1550b0714ce7039270f97a858c4a9b7cb7c8
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-12-15 06:08:25 +0200

    APIDocu:enable2FA

--- src/utils/swagger/swagger.routes.ts
+++ src/utils/swagger/swagger.routes.ts
@@ -31,14 +31,16 @@
  *              description: Bad request
  */
 
+
+
 /**
  * @swagger
- * /resend-verification-email:
- *   put:
- *     summary: Resend verification email for user 
- *     description: Resend the verification email to the user's registered email address.
+ * /enable-2fa-request:
+ *   post:
+ *     summary: Request to enable Two-Factor Authentication (2FA)
+ *     description: Request to enable 2FA for a user account
  *     tags:
- *          - User
+ *       - Authentication
  *     security:
  *       - bearerAuth: []
  *     parameters:
@@ -48,17 +50,43 @@
  *         required: true
  *         schema:
  *           type: string
- *     requestBody:
- *       required: true
- *       content: 
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *           example:
- *             email: user@example.com
  *     responses:
- *        '200':
- *              description: Email sent successfully
- *        '400':
- *              description: Bad request
+ *       '200':
+ *         description: 2FA request sent successfully
+ *       '400':
+ *         description: Bad request
+ */
+
+/**
+ * @swagger
+ * /enable-2fa:
+ *   post:
+ *     summary: Enable Two-Factor Authentication (2FA)
+ *     description: is a specific type of multi-factor authentication (MFA) that requires two forms of identification (also referred to as authentication factors) to grant access to a resource or data. These factors include the traditional method of authentication like username/email and password – plus something you have – like a smartphone – to approve the requests.
+ *     tags:
+ *       - Authentication
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token for authentication
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: Verification Code
+ *         description: Verification code for enabling 2FA
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             code:
+ *               type: string
+ *               description: The verification code for enabling 2FA
+ *     responses:
+ *       '200':
+ *         description: 2FA enabled successfully
+ *       '400':
+ *         description: Bad request or invalid verification code
  */
