3d3c8b024dba23c7ed5e36453ad8b7d32ba921ff
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-12-15 06:10:07 +0200

    APIDocu:requestEnable2FA

--- src/utils/swagger/swagger.routes.ts
+++ src/utils/swagger/swagger.routes.ts
@@ -62,3 +62,35 @@
  *        '400':
  *              description: Bad request
  */
+
+/**
+ * @swagger
+ * /resend-verification-email:
+ *   put:
+ *     summary: Resend verification email for user 
+ *     description: Resend the verification email to the user's registered email address.
+ *     tags:
+ *          - User
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token for authentication
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *           example:
+ *             email: user@example.com
+ *     responses:
+ *        '200':
+ *              description: Email sent successfully
+ *        '400':
+ *              description: Bad request
+ */
