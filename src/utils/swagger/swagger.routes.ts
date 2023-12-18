aa30c262ae017a900360cdd624e16790cf5e19fa
mohamed shehab <74684156+mshehab841@users.noreply.github.com>
2023-12-18 18:01:41 +0200

    Signup and signin doc (#22)
    * Request enable2 fa ap idocu (#12)
    
    * Feat/swagger ap is documentation (#10)
    
    * chore: update user repositery&&model
    
    * docs: initiate swagger docs [updateUser-RE-verif]
    
    * APIDocu:requestEnable2FA
    
    ---------
    
    Co-authored-by: mohamed shehab <74684156+mshehab841@users.noreply.github.com>
    
    * docs: add Swagger documentation for user registration
    
    * docs: add Swagger documentation for user login
    
    ---------
    
    Co-authored-by: Lamiaa Selim <44843176+lamiaaselim@users.noreply.github.com>
    Co-authored-by: Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>

--- src/utils/swagger/swagger.routes.ts
+++ src/utils/swagger/swagger.routes.ts
@@ -150,3 +150,81 @@
  *       '400':
  *         description: Bad request
  */
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               oAuthToken:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
+/**
+ * @swagger
+ * /signin:
+ *   post:
+ *     summary: Sign in user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User signed in successfully
+ *       401:
+ *         description: Unauthorized
+ * /resend-verification-email:
+ *   put:
+ *     summary: Resend verification email for user 
+ *     description: Resend the verification email to the user's registered email address.
+ *     tags:
+ *          - User
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         description: Bearer token for authentication
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *           example:
+ *             email: user@example.com
+ *     responses:
+ *        '200':
+ *              description: Email sent successfully
+ *        '400':
+ *              description: Bad request
+ */
