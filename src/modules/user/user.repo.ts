08c9903c1a6d8e90b5baf6a8af7bdeec9ce6a8b3
Mahmoud Khalid <103892504+MahmoudKhalid22@users.noreply.github.com>
2024-01-09 13:02:03 -0800

    update name (#56)
    Co-authored-by: Muhamed Saleh <72345349+Mohmed-saleh1@users.noreply.github.com>

--- src/modules/user/user.repo.ts
+++ src/modules/user/user.repo.ts
@@ -13,19 +13,25 @@ class userRepo implements userDAO {
     await this.model.create(user);
   }
   async verifyEmail(verificationToken: string): Promise<any> {
-    return await this.model.findOneAndUpdate({ verificationToken }, { verified: true })
+    return await this.model.findOneAndUpdate(
+      { verificationToken },
+      { verified: true }
+    );
   }
   async getUserByEmail(email: string): Promise<User | null> {
-    return await this.model.findOne({ email })
+    return await this.model.findOne({ email });
   }
   async findGoogleId(id: string): Promise<User | null> {
-    return await this.model.findOne({ googleID: id })
+    return await this.model.findOne({ googleID: id });
   }
   async getUserById(id: string): Promise<User | null> {
     return await this.model.findById(id);
   }
+  async updateNameByEmail(email: string, name: string) {
+    const user = await this.model.updateOne({ email }, { name }, { new: true });
+    return user;
+  }
 }
-
 
 type userRepoType = userRepo;
 
